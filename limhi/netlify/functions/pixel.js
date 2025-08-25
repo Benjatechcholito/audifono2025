9906887331e32a2e19ff3daf2fc156aebc36535f
Benjatechcholito <limhibenjaminlozanosalas@gmail.com>
2025-08-25 18:44:29 -0500

    Refactor Facebook Pixel handler for efficiency

--- limhi/netlify/functions/pixel.js
+++ limhi/netlify/functions/pixel.js
@@ -1,40 +1,27 @@
-exports.handler = async (event, context) => {
+// netlify/functions/pixel.js
+exports.handler = async () => {
   const PIXEL_ID = process.env.META_PIXEL_ID;
-  const headers = {
-    'Content-Type': 'application/javascript; charset=utf-8',
-    'Cache-Control': 'public, max-age=300'
-  };
+  if (!PIXEL_ID) return { statusCode: 500, body: '/* META_PIXEL_ID missing */' };
 
-  if (!PIXEL_ID) {
-    return {
-      statusCode: 500,
-      headers,
-      body: '/* META_PIXEL_ID missing */'
-    };
-  }
-
-  const body = `(function(){
-  // Load Facebook Pixel runtime
-  var s = document.createElement('script');
-  s.src = 'https://connect.facebook.net/en_US/fbevents.js';
-  s.async = true;
-  s.onload = function(){
-    try{
-      if (typeof fbq === 'function'){
-        fbq('init','${PIXEL_ID}');
-        fbq('track','PageView');
-      } else if (window.fbq && typeof window.fbq === 'function'){
-        window.fbq('init','${PIXEL_ID}');
-        window.fbq('track','PageView');
-      }
-    }catch(e){/* ignore */}
-  };
-  document.head.appendChild(s);
-})();`;
+  const body = `
+  !(function(f,b,e,v,n,t,s){
+    if(f.fbq) return; n=f.fbq=function(){ n.callMethod ?
+    n.callMethod.apply(n,arguments) : n.queue.push(arguments) };
+    if(!f._fbq) f._fbq=n; n.push=n; n.loaded=!0; n.version='2.0';
+    n.queue=[]; t=b.createElement(e); t.async=!0;
+    t.src=v; s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)
+  })(window, document,'script','https://connect.facebook.net/en_US/fbevents.js');
+  fbq('init', '${PIXEL_ID}');
+  fbq('track', 'PageView');
+  `;
 
   return {
     statusCode: 200,
-    headers,
+    headers: {
+      'Content-Type': 'application/javascript; charset=utf-8',
+      'Cache-Control': 'public, max-age=300'
+    },
     body
   };
 };
